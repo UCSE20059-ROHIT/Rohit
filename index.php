4bf3e715f36941d86528b81b3247af22cfeaa07a
Rohit Ray <83226780+UCSE20059-ROHIT@users.noreply.github.com>
2022-05-09 19:00:26 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,58 +1,42 @@
 <?php
-require_once 'php/DBConnect.php';
-$db = new DBConnect();
-$db->authLogin(); // if user has logged in already then forward it to home.php
+$title = "Home";
+$setHomeActive = "active";
+include 'layout/_header.php';
 
-$message=NULL;
-if(isset($_POST['loginBtn'])){
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    
-    $flag = $db->login($username, $password);
-    if($flag){
-        header("Location: http://localhost/BDManagement/home.php");
-    } else {
-        $message = "Username of password was incorrect!";
-    }
-}
-$title="Login";
-include 'layout/_header.php'; 
+include 'layout/navbar.php';
 ?>
-
 <div class="container">
-<div class="row">
-    <div class="col-md-6">
-        <?php if(isset($message)): ?>
-        <div class="alert-danger"><?= $message; ?></div>
-        <?php endif; ?>
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <div class="col-md-6">
-                    <img src="assets/security-icon.png" class="img img-responsive img-thumbnail">
-                </div>
-                <h5> User Login </h5>
-            </div>
-            <div class="panel-body">
-                <form class="form-vertical" role="form" method="post" action="index.php">
-                    <div class="form-group">
-                        <input type="text" class="form-control" required="true" name="username" placeholder="Username">
-                    </div>
-                    <div class="form-group">
-                        <input type="password" required="true" class="form-control" name="password" placeholder="Password">
-                    </div>
-                    <div class="form-group loginBtn">
-                        <button type="submit" name="loginBtn" class="btn btn-primary btn-sm">Login</button>
-                        <a href="users/" class="btn btn-sm btn-warning">I do not have username or password</a>
-                    </div>
-                </form>
-            </div>
-			</div>
+    <div class="row">
+        <div class="col-md-10">
+            <a href="#" id="Heading">Why Are We Here For?</a>
+            <p id="About">
+                Blood Bank Management System (BBMS) is a browser based system that is designed to store, process, retrieve and analyze information concerned with the administrative and inventory management within a blood bank. This project aims at maintaining all the information pertaining to blood donors, different blood groups available in each blood bank and help them manage in a better way. Aim is to provide transparency in this field, make the process of obtaining blood from a blood bank hassle free and corruption free and make the system of blood bank management effective.
+            </p>
+            <hr id="line_1">
+            <a href="#" id="Heading2">Features--></a>
+            <p>
+
+                - Blood Donation Camp & Camp Organiser Management.<br>
+- Donor Management - Donor Registration, Managing donor database, recording their physical and medical statistics.<br>
+- Inventory management in blood bank for storage and issuance of blood.<br>
+- Blood requisition and issuance of blood.<br>
+- Online transfer of blood from one blood bank to another.<br>
+- Discarding of expired and unsuitable blood (Less Qty., Reactive, Clotting, Hemolysis).<br>
+- Being a web based system, can be implemented throughout the state. - - Separate user accounts can be created for each blood bank.<br>
+- Patient Register/Blood Sample Receiving Register, Donor Register, Blood Issue Register and Discarded Blood report.<br>
+- Fridge Wise Stock Position and Printing of Fridge Stickers.<br>
+- List of Donors who are eligible for donation on a particular date with contact Number.<br>
+- Camp Wise Donor List and Printing of Donor Cards.<br>
+            </p>
         </div>
-    </div>
-    <div class="col-md-4">
-        <img src="assets/left-index-logo.jpg" class="img img-responsive">
+        <div class="col-md-2">
+            <img src="../assets/donate_blood_right.jpg" class="img img-responsive img-thumbnail" style="width:200px ;">
+             <img src="http://bbmspunjab.gov.in/images/bbmsRht.png" class="img img-responsive img-thumbnail">
+        </div>
+
+
+
     </div>
 </div>
 
 <?php include 'layout/_footer.php'; ?>
-
